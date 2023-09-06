a239747fed63e677583e7365d0cbcb3a103540f5
Gabisosa97 <gabisosa97@gmail.com>
2023-09-06 09:03:12 -0300

    + Pruebas de reactividad

--- webapp/Index.js
+++
@@ -1,9 +0,0 @@
-sap.ui.define([
-	"sap/ui/core/mvc/XMLView"
-], function (XMLView) {
-	"use strict";
-
-	XMLView.create({viewName: "project1.App"}).then(function (oView) {
-		oView.placeAt("content");
-	});
-});
--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,37 +1,70 @@
-sap.ui.define([
-	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast",
-	"sap/ui/model/json/JSONModel"
-], function (Controller, MessageToast, JSONModel) {
-	"use strict";
+sap.ui.define(
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel",
+  ],
+  function (Controller, MessageToast, JSONModel) {
+    "use strict";
 
-	return Controller.extend("project1.App", {
-		goForward : function () {
-			MessageToast.show("Bienvenido!");
-			this.byId("app").to(this.byId("intro"));
-		},
-		goBack : function () {
-			this.byId("intro").to(this.byId("content"));
-		},
+    return Controller.extend("project1.controller.App", {
+      goForward: function () {
+        MessageToast.show("Bienvenido!");
+        this.byId("app").to(this.byId("intro"));
+      },
+      goBack: function () {
+        MessageToast.show("Hasta Luego!");
+        this.byId("app").to(this.byId("welcome"));
+      },
 
-		onInit : function () {
-			this.getView().setModel(new JSONModel({
-					features: [
-						"Enterprise-Ready Web Toolkit",
-						"Powerful Development Concepts",
-						"Feature-Rich UI Controls",
-						"Consistent User Experience",
-						"Free and Open Source",
-						"Responsive Across Browsers and Devices"
-					]
-				})
-			);
-		},
+      inc: function (n) {
+        var number = this.getView().byId("input2").getValue();
+        this.getView()
+          .byId("input2")
+          .setValue((+number + 1 * n).toString());
+      },
 
-		onChange: function (oEvent) {
-			var bState = oEvent.getParameter("state");
-			this.byId("ready").setVisible(bState);
-		}
-	});
+      suma: function () {
+        var number1 = this.getView().byId("inputO").getValue() ?? 0;
+        var number2 = this.getView().byId("input1").getValue() ?? 0;
+        this.getView()
+          .byId("input2")
+          .setValue((+number1 + +number2).toString());
+      },
+      op: function () {
+        var sel = this.getView().byId("sel").getSelectedKey();
 
-});
+        console.log(sel);
+        var number1 = this.getView().byId("inputO").getValue() ?? 0;
+        var number2 = this.getView().byId("input1").getValue() ?? 0;
+        this.getView()
+          .byId("input2")
+          .setValue((eval(number1 + sel + number2)));
+      },
+      onInit: function () {
+        this.getView().setModel(
+          new JSONModel({
+            features: [
+              "Enterprise-Ready Web Toolkit",
+              "Powerful Development Concepts",
+              "Feature-Rich UI Controls",
+              "Consistent User Experience",
+              "Free and Open Source",
+              "Responsive Across Browsers and Devices",
+            ],
+          })
+        );
+        this.getView().setModel(
+          new JSONModel({
+            operations: ["+", "-", "*"],
+          })
+        );
+      },
+
+      onChange: function (oEvent) {
+        var bState = oEvent.getParameter("state");
+        this.byId("ready").setVisible(bState);
+      },
+    });
+  }
+);
