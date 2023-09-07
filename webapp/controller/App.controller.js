44981afabbc6b8c02e4a47653cf2a35288ba5ce0
Gabisosa97 <gabisosa97@gmail.com>
2023-09-07 19:37:51 -0300

    + Pruebas de Navegacion y validadores

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -8,14 +8,18 @@ sap.ui.define(
     "use strict";
 
     return Controller.extend("project1.controller.App", {
+
+      // ? NAVIGATION
       goForward: function () {
         MessageToast.show("Bienvenido!");
-        this.byId("app").to(this.byId("intro"));
+        this.byId("app").to(this.byId("calculadora"));
       },
       goBack: function () {
         MessageToast.show("Hasta Luego!");
         this.byId("app").to(this.byId("welcome"));
       },
+
+      // * OPERATIONS
 
       inc: function (n) {
         var number = this.getView().byId("input2").getValue();
@@ -23,23 +27,33 @@ sap.ui.define(
           .byId("input2")
           .setValue((+number + 1 * n).toString());
       },
-
-      suma: function () {
-        var number1 = this.getView().byId("inputO").getValue() ?? 0;
-        var number2 = this.getView().byId("input1").getValue() ?? 0;
-        this.getView()
-          .byId("input2")
-          .setValue((+number1 + +number2).toString());
-      },
       op: function () {
         var sel = this.getView().byId("sel").getSelectedKey();
 
         console.log(sel);
         var number1 = this.getView().byId("inputO").getValue() ?? 0;
         var number2 = this.getView().byId("input1").getValue() ?? 0;
+
+        console.log(number2)
+        console.log(sel)
+        if (number2 && number2 == 0 && sel == "/") {
+          return this.getView().byId("input1").setValueState("Error");
+        } else {
+          this.getView().byId("input1").setValueState("None");
+        }
         this.getView()
           .byId("input2")
           .setValue((eval(number1 + sel + number2)));
+      },
+      conv: function () {
+        var pesos = this.getView().byId("pesos").getValue() ?? 0;
+        this.getView()
+        .byId("impuestos")
+        .setValue((eval(+pesos*1.75 - +pesos)));
+        var impuestos = this.getView().byId("impuestos").getValue() ?? 0;
+        this.getView()
+          .byId("resultado")
+          .setValue((eval(+pesos + +impuestos)));
       },
       onInit: function () {
         this.getView().setModel(
@@ -56,7 +70,7 @@ sap.ui.define(
         );
         this.getView().setModel(
           new JSONModel({
-            operations: ["+", "-", "*"],
+            operations: ["+", "-", "*", "/"],
           })
         );
       },
